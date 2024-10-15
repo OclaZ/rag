2b17b4b9b037284abecd24acf5ae5569a3f843d0
OclaZ <aslikhhamza2001@gmail.com>
2024-10-15 16:23:35 +0100

    first commit

---
+++ app/pinecone/page.tsx
@@ -0,0 +1,12 @@
+import { CardHeader } from "@/components/ui/card";
+import React from "react";
+
+type Props = {};
+
+const VectorDBPage = (props: Props) => {
+  return <main className="flex flex-col items-center p-24">
+    <CardHeader></Card>
+  </main>;
+};
+
+export default VectorDBPage;
